690e732edd59374d7e14fa1e80bc6fa7f6eeeede
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2025-05-20 09:45:42 -0300

    .

---
+++ basicJava/Variables.java
@@ -0,0 +1,12 @@
+package basicJava;
+class Variables{
+    public static void main(String a[]){
+        System.out.println("Hello world");
+        char varChar = 'a';
+        boolean varBool = true;
+        int varInt = 122;
+        float varFloat = 1.22f;
+        double varDouble = 1.22;
+        byte varByte = 12;
+    }
+}
