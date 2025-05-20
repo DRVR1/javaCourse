ca3fa29dbf545b5b62dfb6f6ee2a79e9cf2cab43
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2025-05-20 14:45:25 -0300

    .

--- basicJava/Variables.java
+++ basicJava/Variables.java
@@ -8,5 +8,16 @@ class Variables{
         float varFloat = 1.22f;
         double varDouble = 1.22;
         byte varByte = 12;
+
+        int longNumber = 10_000_000;
+        int binary = 0b1010;
+        int hex = 0x7FA;
+
+        char ch = 'a';
+        char result = (char)(ch + 1);
+        System.out.println(result);
+
+        System.out.println(varInt++); // Notar que sigue siendo 122, por ser un post increment. Esto sirve para bucles, listas, etc. 
+        System.out.println(++varInt); // Ahora si vale 123 (pre increment)
     }
 }
