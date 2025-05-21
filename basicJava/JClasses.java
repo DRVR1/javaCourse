5512a2fc923e7e21a178957476a09a2169c992bb
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2025-05-21 16:36:54 -0300

    .

---
+++ basicJava/JClasses.java
@@ -0,0 +1,36 @@
+package basicJava;
+
+class Calculator{
+    public String name;
+    public Calculator(String name){
+        this.name = name;
+    }
+    public void printName(){
+        System.out.println(this.name);
+    }
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public int multi(int a, int b){
+        return a*b;
+    }
+    public float div(int a, int b){
+        return a/b;
+    }
+    public int mod(int a, int b){
+        return a%b;
+    }
+}
+
+public class JClasses {
+    public static void main(String[] args) {
+        Calculator calc;
+        calc = new Calculator("Braian");
+        calc.printName();
+        System.out.println(calc.add(1,4));
+        System.out.println(calc.mod(10, 3));
+    }
+}
