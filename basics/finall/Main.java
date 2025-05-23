d44f09bf8742f5be71562d0f52c23e4338c02d77
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2025-05-23 17:57:55 -0300

    .

---
+++ basics/finall/Main.java
@@ -0,0 +1,10 @@
+package basics.staticc;
+
+import basics.staticc.Pi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Pi.someNumber);
+        System.out.println(Pi.getValue());
+    }
+}
---
+++ basics/finall/Pi.java
@@ -0,0 +1,9 @@
+package basics.finall;
+
+public final class Pi { // Cannot be inherited
+    public static double someNumber = 3.14; // Cannot be modified
+
+    public static double getValue() {
+        return Math.PI;
+    }
+}
