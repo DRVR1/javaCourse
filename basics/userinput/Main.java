820a78be621739423deed5e4792bebcce8b15d3f
ian <ianvidmar200@gmail.com>
2025-05-28 21:10:19 -0300

    .

--- basics/userinput/Main.java
+++ basics/userinput/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Input your name: ");
         try {
-            System.in.read();
+            int asciiChar = System.in.read();
+            System.out.println(asciiChar);
+
         } catch (Exception e) {
             // TODO: handle exception
         }
