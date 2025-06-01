0b8d6df06922bd5e8e194c4450a5035ee2022005
DRVR1 <ianvidmar200@gmail.com>
2025-06-01 19:07:28 -0300

    .

--- app-name/src/main/java/com/example/app/App.java
+++ app-name/src/main/java/com/example/app/App.java
@@ -1,13 +1,40 @@
 package com.example.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+/* Basic postgresql connection */
+// docker run --name postgres -e POSTGRES_PASSWORD=clave123 -p 5432:5432 -d postgres
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String url = "jdbc:postgresql://localhost:5432/midb";
+        String uname = "postgres";
+        String pass = "clave123";
+
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, uname, pass); // Connect to the DB
+            Statement st = con.createStatement(); // Handle querys to the DB
+            ResultSet result = st.executeQuery("SELECT * FROM STUDENT;");
+
+            result.next(); // Move the cursor to the first row (it starts at index -1). Returns false if no
+                           // row was found
+            System.out.println("Name: " + result.getString("sname"));
+            System.out.println("Age: " + result.getString("sage"));
+
+            // Another way to print results:
+            while (result.next()) {
+                System.out.println("--------------------");
+                System.out.println("Name: " + result.getString("sname"));
+                System.out.println("Age: " + result.getInt("sage"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
