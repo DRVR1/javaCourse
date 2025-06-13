b6c7a5b793fdbd1a970dc9bb63323fed4dfd9558
DRVR1 <ianvidmar200@gmail.com>
2025-06-13 15:51:57 -0300

    .

--- springproject/src/main/java/com/example/Alien.java
+++ springproject/src/main/java/com/example/Alien.java
@@ -1,7 +1,31 @@
 package com.example;
 
 public class Alien {
+    private String name;
+    private int age;
+
     public void code() {
         System.out.println("Coding...");
+    }
+
+    public Alien(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
