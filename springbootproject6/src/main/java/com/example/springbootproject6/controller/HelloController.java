29b19e6880af27ebde106e8583d6490334e5628f
DRVR1 <ianvidmar200@gmail.com>
2025-08-03 12:49:41 -0300

    .

--- springbootproject6/src/main/java/com/example/springbootproject6/controller/HelloController.java
+++ springbootproject6/src/main/java/com/example/springbootproject6/controller/HelloController.java
@@ -4,14 +4,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String greet(HttpServletRequest request) {
-        return "Hello world! " + request.getSession().getId();
+    public String greet(CsrfToken csrfToken, HttpServletRequest request) {
+        return "Hello world! " + request.getSession().getId() + " token: "
+                + csrfToken.getToken();
+    }
+
+    @PostMapping("/post")
+    public String post(HttpServletRequest request) {
+        return "Post";
     }
 
 }
