e8008e9636c085753412745b73ed603e4dcce7f4
DRVR1 <ianvidmar200@gmail.com>
2025-08-19 17:47:34 -0300

    .

--- microservices/microservice1/src/main/java/com/example/microservice1/Microservice1Application.java
+++ microservices/microservice1/src/main/java/com/example/microservice1/Microservice1Application.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.example.microservice1.feing")
 public class Microservice1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Microservice1Application.class, args);
 	}
-
 }
